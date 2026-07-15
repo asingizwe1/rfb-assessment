5087ad6c93d4dd1813977833d242c4479480c0b1
asingizwe1 <asingizwe1louis1@gmail.com>
2026-07-15 10:32:30 +0300

    pretty format restructuring

--- btc-rpc-cli/src/commands/wallet.rs
+++ btc-rpc-cli/src/commands/wallet.rs
@@ -1,9 +1,8 @@
-use anyhow::Result;
-use serde::Deserialize;
-
 use crate::rpc::RpcClient;
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct WalletInfo {
     pub walletname: String,
     pub balance: f64,
@@ -22,10 +21,12 @@ pub fn balance(rpc: &RpcClient) -> Result<()> {
 pub fn execute(rpc: &RpcClient) -> Result<()> {
     let info: WalletInfo = rpc.call("getwalletinfo", &[])?;
 
-    println!("Wallet: {}", info.walletname);
-    println!("Balance: {}", info.balance);
-    println!("Unconfirmed: {}", info.unconfirmed_balance);
-    println!("Transactions: {}", info.txcount);
+    //simpler formatting of output
+    println!("{}", serde_json::to_string_pretty(&info)?);
+    // println!("Wallet: {}", info.walletname);
+    // println!("Balance: {}", info.balance);
+    // println!("Unconfirmed: {}", info.unconfirmed_balance);
+    // println!("Transactions: {}", info.txcount);
 
     Ok(())
 }
